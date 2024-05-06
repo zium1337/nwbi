80db42564707fd08fa90910b7e4a5ec57abd40ac
zium1337 <imflo7dxepij678t7zz4@gmail.com>
2024-05-06 22:29:46 +0200

    update lab3.sh

---
+++ lab3.sh
@@ -0,0 +1,5 @@
+touch lab3.sh
+git add lab3.sh
+git commit -m "init"
+git push --set-upstream https://github.com/zium1337/nwbi.git master
+# Korzystam z metody uwierzytelniania od microsoftu na linuxie do którego personal acceess token nie był potrzebny

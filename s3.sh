22ffd9a4a6356cb3fdf930eed45909b59d989db3
zium1337 <imflo7dxepij678t7zz4@gmail.com>
2024-05-07 11:33:02 +0200

    update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,9 +1,21 @@
-if ["$1" == "--logs"]; then
-    ilosc = "$2"
-    for i in {1..$ilosc}
-    do
-        touch "log${i}.txt"
-    done
-else
-    echo "."
-fi
+#!/bin/bash
+
+arg = "$1"
+
+case $1 in
+    "--date | -d") echo $(date) ;;
+    "--logs | -l")
+        arg2 = "${2:=100}"
+        for i in {1..$arg2}
+        do
+            touch "log${i}.txt"
+        done ;;
+    "--help | -h")
+        echo "--help lub -h | wyswietla ta wiadomosc"
+        echo "--date lub -d | wyswietla dzisiejsza date"
+        echo "--logs [ilosc] lub -l [ilosc] | generuje pliki txt (domyslnie 100)"
+        ;;
+    *)
+        echo "--help lub -h | wyswietla ta wiadomosc"
+        echo "--date lub -d | wyswietla dzisiejsza date"
+        echo "--logs [ilosc] lub -l [ilosc] | generuje pliki txt (domyslnie 100)"

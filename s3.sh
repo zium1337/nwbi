83757b6294571f6c2a128dc13981ffdff11c7aeb
zium1337 <imflo7dxepij678t7zz4@gmail.com>
2024-05-07 12:14:01 +0200

    update s3.sh

--- s3.sh
+++ s3.sh
@@ -15,6 +15,19 @@ case $1 in
         echo "--date lub -d | wyswietla dzisiejsza date"
         echo "--logs [ilosc] lub -l [ilosc] | generuje pliki txt (domyslnie 100)"
         ;;
+    "--init")
+        git clone https://github.com/zium1337/nwbi.git
+        PATH=$PATH:$(pwd)
+        export PATH
+        ;;
+    "--error")
+        arg2 = "${2:=100}"
+        for i in {1..$arg2}
+        do
+            mkdir error$i
+            touch error$i/error$i.txt
+        done
+        ;;
     *)
         echo "--help lub -h | wyswietla ta wiadomosc"
         echo "--date lub -d | wyswietla dzisiejsza date"
